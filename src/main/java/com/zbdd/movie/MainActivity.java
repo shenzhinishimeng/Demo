b260097c65f5db918cd89ea179fc0f1f12f58807
D <1120941208@qq.com>
2017-08-14 20:02:51 +0800

    第一次提交

--- src/main/java/com/zbdd/movie/MainActivity.java
+++ src/main/java/com/zbdd/movie/MainActivity.java
@@ -15,5 +15,8 @@ public class MainActivity extends AppCompatActivity {
 
     private void initView() {
         Log.e("AA","");
+        Log.e("AA","");
+        Log.e("AA","");  Log.e("AA","");
+
     }
 }
